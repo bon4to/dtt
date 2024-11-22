8b394242f3f8acf0b88f277e8f5be78d1082da25
bonato <lucasbon4to@gmail.com>
2024-11-22 17:14:25 -0300

    add player methods

--- src/Player.java
+++ src/Player.java
@@ -2,11 +2,15 @@ public class Player {
     private String nome;
     private int id;
     private int pos;
+    private boolean idle;
+
+    private int turnCounter;
 
     public Player(String nome, int id) {
         this.nome = nome;
         this.id = id;
-        this.pos = 0;
+        this.pos = -1;
+        this.idle = false;
     }
 
     public String getNome() {
@@ -23,5 +27,21 @@ public class Player {
 
     public void setPos(int pos) {
         this.pos = pos;
+    }
+
+    public void toggleIdle(boolean idle) {
+        this.idle = idle;
+    }
+
+    public boolean isIdle() {
+        return idle;
+    }
+
+    public boolean checkTurn() {
+        turnCounter++;
+        if (turnCounter % 4 == 0) {
+            return true;
+        }
+        return false;
     }
 }
